43412a3c8b459087bf62c65e2b58a13e57491dc9
Om-art-web <hariomwkrm@gmail.com>
2024-12-20 15:59:12 +0530

    Quadratic Equations

--- SI.py
+++ SI.py
@@ -1,10 +1,26 @@
-p = input("Please enter principal   ")
-r = input("Please enter rate of interest   ")
-t = input("Please enter time in number of years   ")
-print(type(p))
-p = int(p)
-r = int(r)
-t = int(t)
-print(type(p))
-si = p*r*t/100
-print(si)
+# p = input("Please enter principal   ")
+# r = input("Please enter rate of interest   ")
+# t = input("Please enter time in number of years   ")
+# # print(type(p))
+# p = int(p)
+# r = int(r)
+# t = int(t)
+# # print(type(p))si = p*r*t/100
+# print("SI=",si)
+
+
+
+# ci= p*((1+r/100)**t)-p
+
+# print(ci)
+
+
+p= 100
+t= 2
+r= 5.4
+
+a=p*(1+(r/100))**t
+ci=a-p
+
+print(ci)
+
---
+++ quadratic equation.py
@@ -0,0 +1,38 @@
+# Quadratic equation
+
+# ax**2 + bx +c =0 
+
+'''
+x1= (-b + (b2-4ac)**.5)/(2a)
+x2= (-b - (b2-4ac)**.5)/(2a)
+
+d=b2-4ac
+
+x1= (-b + (d)**.5)/(2a)
+x2= (-b - (d)**.5)/(2a)
+
+
+d=(b2-4ac)**.5
+
+x1= (-b + d/(2a)
+x2= (-b - d/(2a)
+'''
+
+a= input("Enter the number of a  ")
+
+b= input("Enter the number of b  ")
+
+c= input("Enter the number of c ")
+
+
+
+a=1
+b=-5 
+c=6
+
+d=(b**2 - 4*a*c)**.5
+print(d)
+x1=(-b+d)/(2*a)
+
+x2=(-b-d)/(2*a)
+print(x1,x2)
