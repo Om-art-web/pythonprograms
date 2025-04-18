8fde25b002bb74b2ea17473c258f087bf1f18796
Om-art-web <hariomwkrm@gmail.com>
2025-04-18 12:18:30 +0530

    Update START.py

--- Open CV/picture processing/START.py
+++ Open CV/picture processing/START.py
@@ -2,11 +2,10 @@ import cv2 as pp
 
 import imagefunctions as img
     
-p1= "images/Ghost.png"
-p2= "images/mylogo.png"
-pic1 = img.readimage(p1)
-pic2 = img.readimage(p2)
-pic=img.combineimage(pic2,pic1,0,0)
+originalimagepath= "images/Anime.jpg"
+originalimage = img.readimage(originalimagepath)
+img.showimage("",originalimage)
+pic=img.blackandwhite(originalimage)
 img.showimage("",pic)
 # img.saveimage("saveimage",pic)       # Save image
 
